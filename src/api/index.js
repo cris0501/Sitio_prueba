a15b69d84c41e253271c0d02415d739a2f0596a8
Cristian R <cristian050198@outlook.com>
2023-08-03 11:56:34 -0600

    Add: Config API function

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+export default function ({url, method='POST', body=null}){
+    const options = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    if (localStorage.user) options.headers.Authorization = 'Bearer '+JSON.parse(localStorage.getItem('user')).token;
+    if (body) options.body = JSON.stringify(body);
+    
+    const resp = fetch (`${url}`, options)
+    //const resp = fetch (`http://localhost:8000/${url}`, options)
+        .then(resp => {
+            if (resp.status == 200) return resp.json();
+            throw new Error('Error');
+        })
+        .then(resp => {
+            return {
+              status: 200,
+              message: 'OK',
+              data: resp
+            };
+        })
+        .catch((e) => {
+            return {state:'Error', message:'Unknown', data:e}
+        });
+    return resp;
+}
+
